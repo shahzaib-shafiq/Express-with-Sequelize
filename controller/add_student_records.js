aba2bf9a341a1c97fb8a96d583350d4101334a66
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-08-05 14:18:06 +0500

    Add student records

---
+++ controller/add_student_records.js
@@ -0,0 +1,55 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  "sample_student_db",
+  "DATABASE_USERNAME",
+  "DATABASE_PASSWORD",
+  {
+    host: "DATABASE_HOST",
+    dialect: "mysql",
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
+
+const Student = sequelize.define("students", {
+  student_id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+const student_data = [
+  { name: "John Baker" },
+  { name: "Max Butler" },
+  { name: "Ryan Fisher" },
+  { name: "Robert Gray" },
+  { name: "Sam Lewis" },
+];
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    Student.bulkCreate(student_data, { validate: true })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  })
+  .catch((error) => {
+    console.error("Unable to create table : ", error);
+  });
